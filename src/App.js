c6bb8f9dd416f085c3c97a977f9d29fd105fe0d7
Ashutosh <ashutosh.ashu.0520@gmail.com>
2023-07-05 01:03:06 +0530

    err in css

--- src/App.js
+++ src/App.js
@@ -1,45 +1,49 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Todolist from "./components/Todolist";
+// import "./index.css";
 
 function App() {
   const [setInput, currentInput] = useState("");
   const [item, setItem] = useState([]);
-  const setValue = (event)=>{
+  const setValue = (event) => {
     currentInput(event.target.value);
   }
-  const listOfItems = ()=>{
-    setItem((oldItem)=>{
-      
+  const listOfItems = () => {
+    setItem((oldItem) => {
+
       return [...oldItem, setInput];
     })
     currentInput("");
   };
-  const deleteItems = (id)=>{
+  const deleteItems = (id) => {
     console.log("deleted");
-    setItem((oldItems) =>{
-      return oldItems.filter((arrElem, index)=>{
+    setItem((oldItems) => {
+      return oldItems.filter((arrElem, index) => {
         return index !== id;
       });
     });
   };
   return (
     <>
-      
-      <div className="mainbox">
-        <h1>Todo List using React</h1>
-        <input type="text" placeholder="Enter your task" value={setInput} onChange={setValue}/>
-        <button onClick={listOfItems}> + </button>
-        <ol type="I">
-          
-          {item.map( (itemval, index)=>{
-            return <Todolist 
-            key={index}
-            id={index}
-            val={itemval}
-            onSelect={deleteItems}
-            />
-          })}
-        </ol>
+      <div className="mainDiv">
+        <div className="mainbox">
+          <h1>Todo List using React</h1>
+          <div className="inputarea">
+            <input type="text" placeholder="Enter your task" value={setInput} onChange={setValue} />
+            <button className="btn" onClick={listOfItems}> + </button>
+          </div>
+          <ol type="I">
+
+            {item.map((itemval, index) => {
+              return <Todolist
+                key={index}
+                id={index}
+                val={itemval}
+                onSelect={deleteItems}
+              />
+            })}
+          </ol>
+        </div>
       </div>
     </>
   );
