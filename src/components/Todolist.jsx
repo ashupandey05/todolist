03b7be07c8273d06e7fd2abea0b0cf12d009fe4f
Ashutosh <ashutosh.ashu.0520@gmail.com>
2023-07-07 12:17:54 +0530

    css done with responsiveness

--- src/components/Todolist.jsx
+++ src/components/Todolist.jsx
@@ -7,8 +7,8 @@ const Todolist = (props) =>{
     return(
     <>
         <div className="todo-style">
-        <i className="fa-solid fa-xmark" style={setColor} onClick={()=>{props.onSelect(props.id)}}></i>
-             <li>{props.val}</li>
+            <i className="fa-solid fa-xmark fa-2xl" style={setColor} onClick={()=>{props.onSelect(props.id)}}></i>
+            <li>{props.val}</li>
         </div>
     </>
     );
